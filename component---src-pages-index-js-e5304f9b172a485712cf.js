944b6b35941b8047f4287ab4b4fddc168e6f4e2d
sarrrrry.sarrrrry@gmail.com <sarrrrry.sarrrrry@gmail.com>
2020-01-24 16:48:11 +0900

    Updates

--- component---src-pages-index-js-e5304f9b172a485712cf.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Wbzz"),c=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Home"}),l.a.createElement("h1",null,"Hello Tech Blog!"),l.a.createElement("p",null,"Welcome to your new Gatsby site."),l.a.createElement("p",null,"Now go build something great."),l.a.createElement(o.Link,{to:"/page-2/"},"Go to page 2"))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-e5304f9b172a485712cf.js.map
